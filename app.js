c874bb745c8074b7f2af02c21ca726a24ed3e544
Sterling Houghton <sterling.houghton@nike.com>
2016-11-04 07:31:18 -0700

    Add event listeners to controller (enter keypress and and click on description button)

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 	
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
+	
 })();
 
+// GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+
+	var ctrlAddItem = function() {
+
+		console.log('it works')
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if(event.keycode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
 
 })(budgetController, UIController);
