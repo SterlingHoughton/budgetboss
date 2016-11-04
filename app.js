46d8232e1deaae67b26847e16b273abb8d61d157
Sterling Houghton <sterling.houghton@nike.com>
2016-11-04 07:57:20 -0700

    Create setupEventListeners method for initialization function on controller, move event listeners into method

--- app.js
+++ app.js
@@ -33,20 +33,31 @@ var UIController = (function() {
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if(event.keycode === 13 || event.which === 13) {
+				ctrlAddItem() ;
+			}
+		});
+	};
 
 	var ctrlAddItem = function() {
 
 		var input = UICtrl.getInput();
-		console.log(input);
-	}
+		
+	};
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		if(event.keycode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			console.log('App Started.');
+			setupEventListeners();
 		}
-	});
+	};
 
 })(budgetController, UIController);
+
+controller.init();
