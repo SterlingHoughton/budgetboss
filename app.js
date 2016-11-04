5a4296790a9d93f1f8620b92b5e5e1b16a522186
Sterling Houghton <sterling.houghton@nike.com>
2016-11-04 07:50:14 -0700

    Create DOMstrings object for DOM input data, create getInput and getDOMstrings public methods **All on UIController

--- app.js
+++ app.js
@@ -6,18 +6,42 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-	
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	};
+
+	return {
+		getInput: function() {
+			return {
+				type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value
+			};
+		},
+
+		getDOMstrings: function() {
+			return DOMstrings;
+		}
+	};
+
+
 })();
 
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+	var DOM = UICtrl.getDOMstrings();
+
 	var ctrlAddItem = function() {
 
-		console.log('it works')
+		var input = UICtrl.getInput();
+		console.log(input);
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function(event) {
 		if(event.keycode === 13 || event.which === 13) {
